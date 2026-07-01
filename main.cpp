663ea098fc2af83e928399004834780bad52da8f
vantuz236 <vk848111230@gmail.com>
2026-07-01 13:46:44 +0700

    logic fix

--- main.cpp
+++ main.cpp
@@ -6,16 +6,16 @@ int main() {
     Bank bank;
     while (true) {
         parseCommand(player, bank);
-        if (player.months > 3 and player.credit > 0) {
+        if (player.months > 3 && player.credit > 0) {
             std::cout << "You have not repaid your credit in time. Game over." << std::endl;
-            break;
-        }
-        if (player.money > 1000000.0) {
-            std::cout << "Congratulations! You have reached your goal of 1 million. You win!" << std::endl;
             break;
         }
         if (player.credit > 1000000.0) {
             std::cout << "You have taken too much credit. Game over." << std::endl;
+            break;
+        }
+        else if (player.money > 1000000.0) {
+            std::cout << "Congratulations! You have reached your goal of 1 million. You win!" << std::endl;
             break;
         }
     }
